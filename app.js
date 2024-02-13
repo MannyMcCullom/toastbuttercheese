e73b196c0ac8a654c2196f3febab7fa1079990b1
MannyMcCullom <m.mccullom11@gmail.com>
2024-02-13 10:02:01 -0500

    changed link

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const link = "https://drive.google.com/file/d/1r6lUA4g5DYX9pIARhWoIXtO6lZEjsP1p/view?usp=drive_link";
 console.log("here");
 const grid = document.querySelector("#gridContainer");
 const div = document.createElement("a");
 div.classList.add("num");
 div.innerText = "music";
-div.setAttribute("href", "03-King Kunta.zip");
+div.setAttribute("href", link);
 grid.append(div);
