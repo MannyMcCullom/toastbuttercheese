11c0f54ac77213d7b604944a3ac00ccdfe328ca2
Manny McCullom <m.mccullom11@gmail.com>
2024-05-15 14:22:52 -0400

    Overhaul

--- app.js
+++ app.js
@@ -1,25 +1,65 @@
-const music = {"Cheesy Man (MP3)":"https://drive.google.com/drive/folders/1cDy9u0MOV6GGweV4cyCTOMlaoIs6_q9x?usp=drive_link",
+// Goal--------------------------------------------------------------------------------------------------
+
+// Initial Code--------------------------------------------------------------------------------------------
+
+// CheesyMan Album
+const cheesyManLinks = {"Cheesy Man (MP3)":"https://drive.google.com/drive/folders/1cDy9u0MOV6GGweV4cyCTOMlaoIs6_q9x?usp=drive_link",
     "Cheesy Man (FLAC)":"https://drive.google.com/drive/folders/1boWtM_QllTnIOruRUJKCVadOz6qrUXgb?usp=drive_link",
     "Cheesy Man (AIFF)":"https://drive.google.com/drive/folders/1zgegDesKIop46S5iFymYPNIMwx_XMUit?usp=drive_link"
-    };
+};
+const cheesyMan = {"cover": "/Cheesy Man (Album Cover).jpg", "links": cheesyManLinks};
 
-console.log("here");
-const grid = document.querySelector("#gridContainer");
-// const div = document.createElement("a");
-// div.classList.add("num");
-// div.innerText = "music";
-// div.setAttribute("href", links.thing1);
-// grid.append(div);
+const musicCards = document.createElement("div");
+musicCards.classList.add("musicCards");
+document.body.append(musicCards);
 
-function musicLinks (obj) {
-    for (prop in obj) {
-        console.log(obj[prop]);
-        const div = document.createElement("a");
-        div.classList.add("num");
-        div.innerText = prop;
-        div.setAttribute("href", obj[prop]);
-        grid.append(div);
+// Functions--------------------------------------------------------------------------------------------
+
+function removeAllLinks(card) {
+    while (card.firstChild) {
+        card.removeChild(card.firstChild);
     }
 }
 
-musicLinks(music);
+// This function appends each property inside object to specific element.
+function addLinks (obj, el) {
+    for (prop in obj) {
+        console.log(obj[prop]);
+        const div = document.createElement("a");
+        div.classList.add("links");
+        div.innerText = prop;
+        div.setAttribute("href", obj[prop]);
+        el.append(div);
+        const br = document.createElement("br");
+        el.append(br);
+    }
+}
+
+// This function moves card on screen.
+function addCard(el) {
+    el.style.setProperty("left", `0px`);
+}
+
+// This function moves card off screen.
+function removeCard(el) {
+    el.style.setProperty("left", `-500px`);
+}
+
+// Create music card
+function createMusicCard(obj) {
+    const card = document.createElement('div');
+    card.classList.add("card");
+    const cover = document.createElement("img");
+    cover.setAttribute("src", obj["cover"]);
+    cover.classList.add("cover");
+    card.append(cover);
+    const br = document.createElement("br");
+    card.append(br);
+    addLinks(obj["links"], card);
+    musicCards.append(card);
+}
+
+// Running Code--------------------------------------------------------------------------------------------
+
+createMusicCard(cheesyMan);
+// createMusicCard(cheesyMan);
